96a149ccfd1897b1d7ec8556b2e76344d5d16ddd
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-05-30 10:26:37 -0300

    Adiciona módulo __init__.py para definir utilitários do Lumi Assistant

---
+++ src/utils/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Utility modules for Lumi Assistant
+"""
+
+from .patterns import PatternDetector
+from .mood_analyzer import MoodAnalyzer
+
+__all__ = ["PatternDetector", "MoodAnalyzer"]
